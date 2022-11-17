9d09e956ff22e352ba9bdf6797b331cfb03c2329
Thiago Gariani <thiagogarianiq@gmail.com>
2022-11-16 23:36:02 -0300

    ADICIONAR VERSÃO FINAL

---
+++ README.txt
@@ -0,0 +1,10 @@
+Compilação:
+    Após acessar a pasta do projeto, basta executar o seguinte comando no terminal: make main
+
+Execução:
+    Após realizar a compilação, basta executar o comando ./main
+
+Bibliotecas não padrões:
+    pthread.h: define os tipos e as funções usadas para a manipulação de threads.
+    semaphore.h: define os tipos e as funções usadas para a manipulação de semáforos no programa
+    unistd.h: define a função sleep() usada no programa.
--- semaforo.c
+++ semaforo.c
@@ -1,103 +1,125 @@
+/**
+ * @file semaforo.c
+ * @author Thiago Gariani Quinto
+ * @author Iago Ortega Carmona
+ * @author Reginaldo Gregoria de Souza Neto
+ * @brief Programa feito usando semáforos que simula a seguinte situação: o professor está preparando aula de SO, durante esse processo,
+ * ele só pode ser interrompido se todos os alunos de SO chegarem na sala ou se chegar um grupo de dúvidas (não necessariamente de SO).
+ * Se durante o atendimento de dúvidas chegar um novo grupo, este irão esperar o atual atendimento terminar. Os alunos de SO possuem 
+ * sobre os alunos com dúvidas. Quando o último aluno de SO chegar, este irá chamar o professor.
+ *
+ * @version 0.8
+ * Data de criação: 06/11/2022
+ * Date de modificação: 16/11/2022
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
-#include <time.h>
-#include <stdbool.h>
 
 // Defines
-#define N_ALUNOS_SO 40
-#define N_ALUNOS_DUVIDA 10
-#define N_GRUPOS_DUVIDA 3
+#define N_ALUNOS_SO 40 //quantidadde de alunos da turma de SO
+#define N_ALUNOS_DUVIDA 20 //quantidade de alunos com duvida
+#define N_GRUPOS_DUVIDA 3 //quantidade do grupo de dúvidas
 
 // Semaforos
-sem_t sem_dar_aula;
-sem_t sem_atender_aluno;
-sem_t sem_sair_sala;
-sem_t sem_sala_vazia;
-sem_t sem_sala_lotada;
-sem_t sem_dispensar_aluno;
-sem_t sem_prof_livre;
-sem_t sem_atendimento;
-// sem_t sem_grupos_atendidos;
+sem_t sem_dar_aula; //sinalização de professor dar aula
+sem_t sem_atender_aluno; //sinalização de atendimento 
+sem_t sem_sair_sala; //sinalização de que os alunos podem sair da sala de aula
+sem_t sem_sala_vazia; //sinalização de que a sala está de aula vazia
+sem_t sem_sala_lotada; //sinalização de que a sala de aula está lotada
+sem_t sem_dispensar_aluno; //sinalização de que os alunos podem irem embora
+sem_t sem_atendimento; //sinalização de que acabou aatendimento de dúvidas
 
 // Variaveis globais
-int quant_alunos_sala = 0;
-int quant_alunos_duvida = 0;
-int quant_alunos_esperando_sala = 0;
-int quant_oba = 0;
-int cont = 0;
-int prepAula = 0;
-int grupos_duvidas = 0;
-int referencia = 0;
-int alunos_grupo;
-int atend = 0;
+int quant_alunos_sala = 0; //contador da quantidade de alunos de SO na sala
+int quant_alunos_duvida = 0; //contador da quantidade de alunos om dúvida
+int quant_alunos_esperando_sala = 0; //contador da quantidade de alunos de SO esperando na sala 
+int prepAula = 0; //variável que sinaliza que o professor começou a preparar a aula
+int grupos_duvidas = 0;//contador de grupos com dúvida
+int alunos_grupo; //contador para verificar se o grupo foi atendido
+int iniAulaSO = 0; //variável para mostrar que a aula começou
+int alunosDuvidasFila;
 int i;
+int atendimento; //verificação se novo grupo pode ir
 
 // Instancias de funcoes
-void *professor(void *arg);
+void *professor();
 void *aluno_so(void *arg);
 void *aluno_duvida(void *arg);
 
-// Funcoes
-void *professor(void *arg){
+/****************************************************PROFESSOR*****************************************************************/
+
+/**
+ * @brief função que o professor realiza, na qual ele verifica se oque fez com que ele parasse de preparar aula. Além disso, 
+ * ele espera pela sinalização de que os alunos chegaram, após isso, dá início à aula e após o fim sinaliza aos alunos que 
+ * eles podem ir embora. Para ir embora o professor espera a saída de todos os alunos da sala.
+ * 
+ * @param arg 
+ * @return void* 
+ */
+void *professor(){
     
     //Preparar Aula
-        printf("O professor Campiolo esta preparando atividades em sua sala!\n\n");
+        printf("👨‍🏫O professor esta preparando atividades em sua sala!\n\n");
         prepAula = 1;
-        
-    //if(cont == 0){
-     //   cont++;
-    //sem_post(&sem_prof_livre);
-    //}
-
+  
     //fica no laço esperando se algo vai mudar seu estado
     while(quant_alunos_esperando_sala < N_ALUNOS_SO && quant_alunos_duvida < N_ALUNOS_DUVIDA){}
 
     //verifica oque resultou na mudança de estado, se foi a chegada de alunos com dúvida entra no IF 
     if(quant_alunos_duvida == N_ALUNOS_DUVIDA){
-        //atende alunos enquanto tiverem grupos de dúvidas
+        
         while(grupos_duvidas > 0 || quant_alunos_esperando_sala < N_ALUNOS_SO){  
-            //verifica se novo grupo chegou         
-            if(grupos_duvidas > referencia){
-                printf("Professor vai atender os grupos de dúvidas\n\n");
-                referencia = grupos_duvidas;
-            }
+            
+            //fica no laço enquanto uma das condições forem satisfeitas
         }
+        
     } 
 
         sem_post(&sem_atendimento); //sinaliza de que acabou atendimento
 
         sem_wait(&sem_sala_lotada); //professor espera pela sinalização de chegada de todos os alunos
-        printf("O professor saiu para dar aula.\n\n");
-        sleep(1);
-
-        printf("O professor vai começar a aula!\n\n"); 
+        
+        printf("👨‍🏫 O professor vai começar a aula!\n\n"); 
         sleep(2);
 
         for(i = 1; i <= N_ALUNOS_SO; i++){
-            sem_post(&sem_dar_aula);
+            sem_post(&sem_dar_aula); //sinaliza para os alunos de SO que a aula vai começar
         }
 
-        printf("A aula acabou, vou liberar os alunos!\n\n");
-        sleep(1);
+        printf("👨‍🏫 A aula acabou, vou liberar os alunos!\n\n");
+        sleep(2);
 
         for(i = 1; i <= N_ALUNOS_SO; i++){
-            sem_post(&sem_dispensar_aluno);
+            sem_post(&sem_dispensar_aluno); //sinalização para os alunos irem embora
         }
 
-        // Dispensar alunos
-        sem_wait(&sem_sala_vazia);
-        printf("O professor vai embora para casa!\n\n");
+        sem_wait(&sem_sala_vazia); //espera pela sinalização de que a sala está vazia
+        printf("👨‍🏫 O professor vai embora para casa!\n\n");
 
-        return 0;
+        pthread_exit(0);
 }
 
+
+/***********************************ALUNOS SO************************************************************/
+
+/**
+ * @brief função que contém o que os alunos de SO irão fazer, as ação realizadas por eles são a sinalização
+ * ao professor de que todos chegaram para a aula, além disso, sinalizam também ao professor que todos foram 
+ * embora ao final da aula. Quando o professor sinaliza o início da aula, todos exclamam sua felicidade.
+ * 
+ * @param arg ID do aluno de SO
+ * @return void* 
+ */
 void *aluno_so(void *arg){
 
     while(!prepAula){}
 
+    //geração aleatória de alunos de SO
     int id_so = *(int*)arg;
     sleep(3);
     srand(time(NULL) + (id_so * 2));
@@ -105,42 +127,56 @@ void *aluno_so(void *arg){
     sleep(sleepTime);
 
     // Aluno de SO entra na sala
-    printf("\tAluno_SO_%d entrou na sala\n\n", id_so);
+    printf("\t👨‍💻 Aluno_SO_%d entrou na sala\n\n", id_so);
     quant_alunos_sala++;
     
     // Aluno de SO aguarda o inicio da aula
-    printf("\tAluno_SO_%d esta aguardando a aula\n\n", id_so);
+    printf("\t👨‍💻 Aluno_SO_%d esta aguardando a aula\n\n", id_so);
     quant_alunos_esperando_sala++;
-    printf("\tAluno_SO %d / %d\n\n", quant_alunos_esperando_sala, N_ALUNOS_SO);
 
     if(quant_alunos_esperando_sala == N_ALUNOS_SO){
         printf("\tA SALA ESTA CHEIA!\n\n");     
-        //sem_wait(&sem_prof_livre);
         sem_wait(&sem_atendimento); //espera pela sinalização de que acabou atendimento
-        sem_post(&sem_sala_lotada);
-        printf("\tO Aluno_SO_%d foi chamar o professor.\n\n", id_so);
-        //sem_post(&sem_prof_livre);
+        iniAulaSO = 1; //motra que aula começou
+        alunosDuvidasFila = (grupos_duvidas * N_GRUPOS_DUVIDA) - quant_alunos_duvida;
+        if(alunosDuvidasFila > 0){
+            for(int i = 1; i <= alunosDuvidasFila; i++){
+                sem_post(&sem_atender_aluno);
+            }
+        }
+        sem_post(&sem_sala_lotada); //sinaliza que a sala de aula está cheia
+        printf("\t👨‍💻 O Aluno_SO_%d foi chamar o professor.\n\n", id_so);
     }
 
-    sem_wait(&sem_dar_aula);
-    printf("\tOba aula de SO! %d\n\n", id_so);
+    sem_wait(&sem_dar_aula); //espera pela sinalização de que a aula vai começar
+    printf("\t👨‍💻 Oba aula de SO! %d\n\n", id_so);
    
-    sem_wait(&sem_dispensar_aluno);
-    printf("\tAluno %d saiu da sala!\n\n", id_so);
-    quant_alunos_sala--;
+    sem_wait(&sem_dispensar_aluno); //espera pela sinalização de uqe pode dair da sala
+    printf("\t👨‍💻 Aluno %d saiu da sala!\n\n", id_so);
+    quant_alunos_sala--; //decrementa contador de alunos de SO na sala
 
-    if(quant_alunos_sala == 0){
+    if(quant_alunos_sala == 0){ //se todos os alunos foram embora, sinaliza para o professor 
         printf("\tSALA VAZIA!!!\n\n");
-        sem_post(&sem_sala_vazia);
+        sem_post(&sem_sala_vazia); //sinaliza para o professor que a sala de aula está vazia
     }
-    //sleep(1);
-    return NULL;
+    
+    pthread_exit(0);
 }
 
+
+/********************************************************ALUNOS DÚVIDA*************************************************************/
+/**
+ * @brief função que contém as ações que os aluno com dúvidas irão realizar, como o aguardo da atendimento
+ * do professor, ao terminarem de serem atendidos, o grupo atual s
+ * 
+ * @param arg ID do aluno com dúvidas
+ * @return void* 
+ */
 void *aluno_duvida(void *arg){
 
     while(!prepAula){}
     
+    //geração aleatória
     int id_duvida = *(int*)arg;
     sleep(3);
     srand(time(NULL) + (id_duvida * 2));
@@ -148,75 +184,66 @@ void *aluno_duvida(void *arg){
     sleep(sleepTime);
 
     //Aluno com duvida chega para falar com o professor
-    printf("\t\tAluno_DUVIDA_%d chegou na sala do professor\n\n", id_duvida);
-    quant_alunos_duvida++;
+    printf("\t\t🙋‍ Aluno_DUVIDA_%d chegou na sala do professor\n\n", id_duvida);
+    quant_alunos_duvida++; //incrementa quantidade de alunos com dúvida
 
-     printf("\t\tAluno_DUVIDA_%d esta aguardando o professor\n\n", id_duvida);
-    
-    //módulo
-    //if(quant_alunos_duvida == N_GRUPOS_DUVIDA){
-
-    
-    if(quant_alunos_esperando_sala == N_ALUNOS_SO){
-        printf("\t\tTodos os alunos de SO chegaram, assim, Aluno_Duvida_%d vai embora\n\n", id_duvida);
-        quant_alunos_duvida--;
-        pthread_exit(NULL);
+     printf("\t\t🙋‍ Aluno_DUVIDA_%d esta aguardando o professor\n\n", id_duvida);
+   
+    if(quant_alunos_esperando_sala == N_ALUNOS_SO){ //se todos os alunos de SO já chegaram, aluno com dúvida vai embora
+        printf("\t\t🙋‍ Todos os alunos de SO chegaram, assim, Aluno_Duvida_%d vai embora\n\n", id_duvida);
+        quant_alunos_duvida--; //decrementa contador de alunos com dúvida
+        pthread_exit(0);
     }
 
     
-    if((quant_alunos_duvida % N_GRUPOS_DUVIDA) == 0){
+    if((quant_alunos_duvida % N_GRUPOS_DUVIDA) == 0){ //verifica se há um grupo com dúvidas
 
-        alunos_grupo = N_GRUPOS_DUVIDA;
-        grupos_duvidas++;
+        alunos_grupo = N_GRUPOS_DUVIDA; //atribui para a variável o número que contém o número de alunos do grupo
+        grupos_duvidas++; //incrementa variável de grupos com dúvida
+        while(atendimento){
+            //espera que novo grupo termine
+        } 
         printf("\t\tO grupo de duvidas lotou!\n\n");
-        
+        printf("👨‍🏫 Professor vai atender o grupo!\n\n");
         for(i = 1; i <= N_GRUPOS_DUVIDA; i++){
-            sem_post(&sem_atender_aluno);
+            sem_post(&sem_atender_aluno); //permite aos alunos com dúvida irem tirar suas dúvidas
+        }
+        atendimento = 1; //mostra que há um grupo que será atendido
+    }
+
+        sem_wait(&sem_atender_aluno); //espera pela sinalização de que é possível tirar dúvidas
+        if(iniAulaSO){
+            printf("\t\t🙋‍ Todos os alunos de SO chegaram, assim, Aluno_Duvida_%d vai embora\n\n", id_duvida);
+            quant_alunos_duvida--;
+            pthread_exit(0);
         }
 
-        //sem_wait(&sem_prof_livre);
-        //sem_post(&sem_atender_aluno);
-    }
-
-        sem_wait(&sem_atender_aluno);
-        printf("\t\tAluno_DUVIDA %d foi atendido!\n\n", id_duvida);
-            alunos_grupo--;
-        if(alunos_grupo == 0){
-            grupos_duvidas--;
+        printf("\t\t🙋‍ Aluno_DUVIDA %d foi atendido!\n\n", id_duvida);
+            alunos_grupo--; //foi atendido, decrementa contador
+        if(alunos_grupo == 0){ //todo o grupo foi atendido
+            atendimento = 0; //novo grupo pode ir 
+            grupos_duvidas--; //decrementa contador de grupo de dúvidas
             printf("\t\tGRUPO ATENDIDO!!!\n\n");
-            //sem_post(&sem_prof_livre);
+            
         } 
-        
-        /*
-        else{
-            
-            for(i = 1; i <= N_GRUPOS_DUVIDA; i++){
-                sem_post(&sem_atender_aluno);
-            }
-            
-        }   
-        */ 
+    
+        pthread_exit(0);
 }
 
 int main(){
     
-    // Inicialização dos semaforos
+    // Inicializa os semáforos para sinalização
     sem_init(&sem_atender_aluno, 0, 0);
     sem_init(&sem_sair_sala, 0, 0);
     sem_init(&sem_sala_vazia, 0, 0);
     sem_init(&sem_sala_lotada, 0, 0);
     sem_init(&sem_dispensar_aluno, 0, 0);
-    sem_init(&sem_prof_livre, 0, 0);
     sem_init(&sem_atendimento, 0 , 0);
-    // sem_init(&sem_grupos_atendidos, 0, 0);
 
-    // Criação das threads
     pthread_t professor_thread;
     pthread_t aluno_so_thread[N_ALUNOS_SO];
     pthread_t aluno_duvida_thread[N_ALUNOS_DUVIDA];
 
-
-    // Cria argumentos para as threads
     int i;
     int *id_aluno_so;
     int *id_aluno_duvida;
@@ -236,7 +263,6 @@ int main(){
         pthread_create(&aluno_so_thread[i], NULL, aluno_so, (void*) id_aluno_so);
     }
     
-
     // Espera pelas threads
     pthread_join(professor_thread, NULL);
 
